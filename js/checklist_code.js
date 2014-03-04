8289be12f4f0370a3157e5176e5e04b31ff731de
Brian Dean <bridean@gmail.com>
2014-03-04 14:57:11 -0800

    CodeFellows Test Files Added
    My files for UX Engineering test project - Chocolate Pizza recipe page
    with interactive checklist

---
+++ js/checklist_code.js
@@ -0,0 +1,77 @@
+$(document).ready(function() {
+	
+	$("#cb01").click(function() {
+		$(this).attr("src", "images/cb01-checked.jpg");
+		
+/*		function(){this.src = this.src.replace("01","01-checked");},
+          function(){this.src = this.src.replace("01-checked","01");}
+		
+		 var audio = {};
+        audio["walk"] = new Audio();
+        audio["walk"].src = "scratch.wav"
+        audio["walk"].addEventListener('load', function () {
+            audio["walk"].play();
+            });
+		*/
+	});
+
+	$("#cb02").click(function() {
+		$(this).attr("src", "images/cb02-checked.jpg");
+	});
+
+	$("#cb03").click(function() {
+		$(this).attr("src", "images/cb03-checked.jpg");
+	});
+
+	$("#cb04").click(function() {
+		$(this).attr("src", "images/cb04-checked.jpg");
+	});
+
+	$("#cb05").click(function() {
+		$(this).attr("src", "images/cb05-checked.jpg");
+	});
+
+	$("#cb06").click(function() {
+		$(this).attr("src", "images/cb06-checked.jpg");
+	});
+
+	$("#cb07").click(function() {
+		$(this).attr("src", "images/cb07-checked.jpg");
+	});
+
+	$("#cb08").click(function() {
+		$(this).attr("src", "images/cb08-checked.jpg");
+	});
+
+	$("#cb09").click(function() {
+		$(this).attr("src", "images/cb09-checked.jpg");
+	});
+
+	$("#cb10").click(function() {
+		$(this).attr("src", "images/cb10-checked.jpg");
+	});
+
+	$("#cb11").click(function() {
+		$(this).attr("src", "images/cb11-checked.jpg");
+	});
+
+	$("#cb12").click(function() {
+		$(this).attr("src", "images/cb12-checked.jpg");
+	});
+
+
+
+/*  SAVED FOR IN CASE MISTAKE HAS BEEN MADE
+	$("h1").click(function() {
+		$("#cb01").attr("src", "images/cb01-checked.jpg");
+	});
+*/
+
+
+/*
+  $("h1").click(function() {
+    $("h2").fadeTo(700, 0.0);
+  });
+  */
+  
+});
