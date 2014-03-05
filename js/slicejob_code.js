ca842cf9c0b4481ad3cf552b2dac81ce7c9ec3c3
Brian Dean <bridean@gmail.com>
2014-03-05 15:54:01 -0800

    Enhancement of test files
    I've redone my work because at first I didn't realize the web page had
    to be more than just a sliced up screenshot with some interactivity, it
    now functions more like a web page; wasn't certain at first whether
    instructors wanted much time put into formatting the page into nicely
    divided graphics & text

---
+++ js/slicejob_code.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+		
+//below: when h1 tag is clicked, this function is triggered that _______	
+	
+	$("li").click(function() {
+		$(this).toggleClass("li-unchecked li-checked");
+	});
+
+	
+/*	$("li").toggleClass("li-unchecked li-checked");
+*/	
+	
+});
